4ed7c7b5293d46f79b84b66d3502b39652a80b23
Shitanshu Tripathi <tripathis1106@gmail.com>
2024-02-07 22:08:08 +0530

    Added Utils

---
+++ backend/src/utils/asyncHandler.js
@@ -0,0 +1,8 @@
+//Creating higher-order function
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export {asyncHandler}
