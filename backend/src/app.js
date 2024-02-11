cae8d6581fa9c6abb560514d4d81b12392c7ded1
Shitanshu Tripathi <tripathis1106@gmail.com>
2024-02-11 18:45:53 +0530

    App.js file updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,24 @@
 import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 const app = express();
+
+app.use(cors({
+    origin : process.env.CORS_ORIGIN,
+    credentials : true
+}));
+
+//Providing limit for JSON data to prevent server from crashing
+app.use(express.json({limit: "16kb"}))
+
+//URL data configuration for special characters and white spaces
+app.use(express.urlencoded({extended: true, limit: "16kb"}));
+
+//Storing assets such as favicon and images in 'public' folder
+app.use(express.static("public"))
+
+//Accessing and setting (CRUD) cookies on client's browser
+app.use(cookieParser())
 
 export { app }
